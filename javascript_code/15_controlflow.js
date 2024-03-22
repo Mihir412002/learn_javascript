b15608bf33b662eac7aea2de1533c638c66ad5c8
Mihir412002 <100696658+Mihir412002@users.noreply.github.com>
2024-03-22 14:17:55 +0000

    ok

---
+++ javascript_code/15_controlflow.js
@@ -0,0 +1,92 @@
+"using strict"
+
+//if
+
+// if(2==2){
+//     console.log("true");
+// }
+
+// if(2 == "2"){//true
+//     console.log("true");
+// }
+
+// if(2 === "2"){//false
+//     console.log("false");
+// }
+
+
+//<,>,<=,>=,==,===,!=,!==
+
+
+// const temp = 50
+
+// if(temp > 50){
+//     console.log("very hott");
+// }
+// else{
+//     console.log("avaerage hot");
+// }
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`if score is ${power} you can fly`);
+// }
+
+// console.log(`fly when score is ${power}`);//you cant accees out pff scope
+
+// const balance = 1000
+
+//if you not writing curly braces then scope will be called implcite scope and should be written in one line like
+
+// if(balance > 500)  console.log("ok fine");
+
+// if(balance > 500)  console.log("ok fine"),
+// console.log("good");
+
+
+// const shape = "ciricle"
+
+// if(shape == "rectnable"){
+//     console.log("rectangle");
+// }
+// else if(shape == "square"){
+//     console.log("square");
+// }
+// else if(shape == "cone"){
+//     console.log("cone");
+// }
+// else{
+//     console.log("yes circle");
+// }
+
+
+const a = true
+const b = true
+
+// if(a && b){
+//     console.log("you can buy");
+// }
+
+// if(a && b && 2==2){
+//     console.log("you can buy");
+// }
+
+
+
+// if(a && b && 2==3){
+//     console.log("you can buy");
+// }
+
+
+
+// 
+
+
+
+
+
+
+
+
